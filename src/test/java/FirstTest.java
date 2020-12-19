c163cb86513a0bdf6f686c0c84e678b96e09cd0e
Zzzezich <sergej.karpenko.1991@mail.ru>
2020-12-19 18:23:21 +0700

    Second commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,51 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void openPage() {
+        String titleActual = driver.getTitle();
+        String titleExpected = "Unlimint Payment Page";
+        Assert.assertEquals("Title not matching", titleExpected, titleActual);
+    }
+
+    @Test
+    public void firstPayman() {
+        WebElement cardNumber = driver.findElement(By.id("input-card-number"));
+        cardNumber.sendKeys("4000000000000002");
+        WebElement cardHolder = driver.findElement(By.id("input-card-holder"));
+        cardHolder.sendKeys("Ci Von");
+        WebElement selectMonth = driver.findElement(By.id("card-expires-month"));
+        Select selectM = new Select(selectMonth);
+        selectM.selectByVisibleText("02");
+        WebElement selectYear = driver.findElement(By.id("card-expires-year"));
+        Select selectY = new Select(selectYear);
+        selectY.selectByVisibleText("2033");
+        WebElement CVV = driver.findElement(By.id("input-card-cvc"));
+        CVV.sendKeys("123");
+        WebElement PayButton = driver.findElement(By.id("action-submit"));
+        PayButton.click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+}
